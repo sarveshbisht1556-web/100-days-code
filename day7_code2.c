d3156489e684ee0b8c13a3b6bbc03b8a43d160a5
Sarvesh Bisht <sarveshbisht1556@gmail.com>
2026-08-16 21:25:55 +0530

    Update day7_code2.c
    Added comments for clarity and improved code readability.

--- day7_code2.c
+++ day7_code2.c
@@ -3,19 +3,21 @@
 #include<stdio.h>
 int main()
 {
- char alpha;
+ char alpha;     //Declaring input variable for alphabets.
 
  printf("Enter the alphabet to check whether it is a consonant or a vowel : ");
  
- scanf("%c" , &alpha);
+ scanf("%c" , &alpha);      //Taking input from the user.
+
+ // Simple condition for vowels.
  
  if ( alpha == 'a' || alpha =='e' || alpha =='i' || alpha =='o' || alpha =='u' || alpha == 'I' || alpha =='A' || alpha =='E' || alpha =='O' || alpha =='U')
 {  
- printf("Vowel");
+ printf("Vowel");   // Displaying output for vowels.
 }
  else
 {
- printf("Consonant");
+ printf("Consonant");  //Displaying output for consonants.
 }
 return 0;
 
